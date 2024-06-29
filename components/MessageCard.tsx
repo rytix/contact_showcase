4fc7a2643721cef60782bcf8dadfda4e04b68a76
rafaeldgruenevald <rafaeldgruenevald@proton.me>
2024-06-29 19:24:19 -0300

    fix padding and stuff of user page

--- components/MessageCard.tsx
+++ components/MessageCard.tsx
@@ -1,12 +1,12 @@
 export default function MessageCard() {
   return (
     <>
-      <div className="flex flex-col sm:flex-row shadow-xl rounded-3xl border-2 border-solid border-gray-300 h-44 w-full mb-5">
+      <div className="flex flex-col sm:flex-row shadow-xl rounded-3xl border-2 border-solid border-gray-300 h-48 w-full mb-5">
         <div className="flex flex-col justify-center items-center h-full w-full sm:items-baseline sm:w-2/5 md:1/5 ml-2">
           <h1 className="font-bold text-xl">Nome da Pessoa</h1>
           <h2 className="font-medium text-lg">São Paulo, SP</h2>
         </div>
-        <div className="h-full w-full sm:w-3/5 md:w-4/5 text-xs md:text-sm mt-2">
+        <div className="h-full w-full sm:w-3/5 md:w-4/5 text-xs md:text-sm p-2 text-justify">
           <p className="mb-2">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
             eum odit, consequatur recusandae tenetur rem culpa aliquid quae,
--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -9,7 +9,7 @@ export default function UserInfo({ status, dataCadastro }: PropsUserInfo) {
   return (
     <>
       <div className="shadow-xl rounded-3xl border-2 border-solid border-gray-300 sm:h-80 h-82 w-full">
-        <div className="flex flex-col sm:flex-row sm:h-40 h-full w-full sm:mt-5">
+        <div className="flex flex-col sm:flex-row sm:h-40 h-full w-full mt-5 p-2 md:p-0">
           <div className="order-1 flex flex-col justify-center items-center h-1/3 w-full sm:h-full sm:w-1/3 sm:mt-5">
             <img
               src="https://picsum.photos/200/200"
